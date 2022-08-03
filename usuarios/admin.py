582634ef1474c7afd4cc83023d62999df85359c2
NoahTakk11 <94125676+NoahTakk11@users.noreply.github.com>
2022-08-02 22:08:36 -0300

    ignorando databases

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Usuario, AsistenciaUsuario
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(AsistenciaUsuario)
